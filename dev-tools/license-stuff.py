320027f8b266eef4d4ab68691fe87cc49ffe0e9c
Stig Rohde Døssing <srdo@apache.org>
2019-04-17 23:41:29 +0200

    WIP

--- dev-tools/license-stuff.py
+++ dev-tools/license-stuff.py
@@ -29,6 +29,8 @@ import re
 import itertools
 
 project_root = Path(__file__).resolve().parent.parent
+update_dependency_licenses_cmd = ('mvn license:aggregate-add-third-party@generate-and-check-licenses' +
+                                  ' -Dlicense.skipAggregateAddThirdParty=false')
 
 
 @contextmanager
@@ -40,18 +42,22 @@ def cd(newdir):
     finally:
         os.chdir(prevdir)
 
+# Generates DEPENDENCY-LICENSES in target. The committed DEPENDENCY-LICENSES is not modified.
 
-def check_dependency_licenses():
-    # Verify DEPENDENCY-LICENSES is up to date
-    print('Checking DEPENDENCY-LICENSES')
-    update_dependency_licenses_cmd = ('mvn license:aggregate-add-third-party@generate-and-check-licenses' +
-                                      ' -Dlicense.skipAggregateAddThirdParty=false')
+
+def generate_dependency_licenses():
+    print('Generating DEPENDENCY-LICENSES')
     update_dependency_licenses_output_to_target_cmd = (update_dependency_licenses_cmd +
                                                        ' -Dlicense.thirdPartyFilename=DEPENDENCY-LICENSES' +
                                                        ' -Dlicense.outputDirectory=target')
-
     subprocess.check_call(shlex.split(
         update_dependency_licenses_output_to_target_cmd))
+
+# Regenerates DEPENDENCY-LICENSES, and verifies that it didn't change
+
+
+def check_dependency_licenses():
+    print('Checking DEPENDENCY-LICENSES')
     if (not filecmp.cmp(Path('DEPENDENCY-LICENSES'), Path('target') / 'DEPENDENCY-LICENSES', shallow=False)):
         print(
             f"DEPENDENCY-LICENSES and target/DEPENDENCY-LICENSES are different. Please update DEPENDENCY-LICENSES by running '{update_dependency_licenses_cmd}' in the project root")
@@ -66,43 +72,70 @@ def build_storm():
     ))
 
 
-def build_storm_dist_binary():
-    print("Building storm-dist/binary")
-    with cd(project_root / 'storm-dist' / 'binary'):
-        subprocess.check_call(shlex.split('mvn clean package -Dgpg.skip'))
-
-
-def extract_dependency_report_maven_coordinates(lines):
+def extract_license_report_maven_coordinates(lines):
     # Lines like " * Checker Qual (org.checkerframework:checker-qual:2.5.2 - https://checkerframework.org)"
     matches = map(lambda line: re.match(
         r'\s+\*.*\((?P<gav>.*) \- .*\).*', line), lines)
     return set(map(lambda match: match.group('gav'), filter(lambda match: match != None, matches)))
+
+# Gets the dependencies listed in LICENSE-binary
+
+
+def parse_license_binary_dependencies_coordinate_set():
+    license_binary_begin_binary_section = '----------------------------END OF SOURCE NOTICES -------------------------------------------'
+    license_binary_lines = read_lines(project_root / 'LICENSE-binary')
+    return extract_license_report_maven_coordinates(
+        itertools.dropwhile(lambda line: license_binary_begin_binary_section not in line, license_binary_lines))
+
+
+def extract_dependency_list_maven_coordinates(lines):
+    # Lines like "   com.google.code.findbugs:jsr305:jar:3.0.2"
+    matches = map(lambda line: re.match(
+        r'\s+(?P<group>.*)\:(?P<artifact>.*)\:(?P<type>.*)\:(?P<version>.*)', line), lines)
+    return set(map(lambda match: match.group('group') + ':' + match.group('artifact') + ':' + match.group('version'), filter(lambda match: match != None, matches)))
 
 
 def read_lines(path):
     with open(path) as f:
         return f.readlines()
 
+# Gets the dependencies for storm-dist/binary, plus the ones in storm-shaded-deps
+
+
+def generate_storm_dist_dependencies_coordinate_set():
+    generated_coordinate_set = extract_license_report_maven_coordinates(read_lines(
+        project_root / 'storm-dist' / 'binary' / 'target' / 'generated-sources' / 'license' / 'THIRD-PARTY.txt'))
+
+    # Add dependencies from storm-shaded-deps
+    with cd(project_root / 'storm-shaded-deps'):
+        subprocess.check_call(shlex.split(
+            'mvn dependency:list -DoutputFile=target/deps-list -Dmdep.outputScope=false -DincludeScope=compile'))
+    shaded_dep_coordinates = extract_dependency_list_maven_coordinates(
+        read_lines(project_root / 'storm-shaded-deps' / 'target' / 'deps-list'))
+    print('Adding storm-shaded-deps dependencies ' + str(shaded_dep_coordinates))
+    print('')
+    generated_coordinate_set.update(shaded_dep_coordinates)
+
+    return set(filter(lambda coordinate: 'org.apache.storm:' not in coordinate, generated_coordinate_set))
 
 # Checks that the dependencies in the storm-dist/binary report are mentioned in LICENSE-binary, and vice versa
+
+
 def check_license_binary():
     print('Checking LICENSE-binary')
     with cd(project_root / 'storm-dist' / 'binary'):
         print('')
-        #subprocess.check_call(shlex.split('mvn license:aggregate-add-third-party@generate-and-check-licenses -Dlicense.skipAggregateAddThirdParty=false'))
-    generated_coordinate_set = extract_dependency_report_maven_coordinates(read_lines(
-        project_root / 'storm-dist' / 'binary' / 'target' / 'generated-sources' / 'license' / 'THIRD-PARTY.txt'))
-    generated_coordinate_set_without_storm_artifacts = set(filter(lambda coordinate: 'org.apache.storm:' not in coordinate, generated_coordinate_set))
-        
-    license_binary_begin_binary_section = '----------------------------END OF SOURCE NOTICES -------------------------------------------'
-    license_binary_lines = read_lines(project_root / 'LICENSE-binary')
-    license_binary_coordinate_set = extract_dependency_report_maven_coordinates(
-        itertools.dropwhile(lambda line: license_binary_begin_binary_section not in line, license_binary_lines))
-    superfluous_coordinates_in_license = license_binary_coordinate_set.difference(generated_coordinate_set_without_storm_artifacts)
-    coordinates_missing_in_license = generated_coordinate_set_without_storm_artifacts.difference(license_binary_coordinate_set)
+        subprocess.check_call(shlex.split(
+            'mvn license:aggregate-add-third-party@generate-and-check-licenses -Dlicense.skipAggregateAddThirdParty=false'))
+
+    license_binary_coordinate_set = parse_license_binary_dependencies_coordinate_set()
+    generated_coordinate_set = generate_storm_dist_dependencies_coordinate_set()
+    superfluous_coordinates_in_license = license_binary_coordinate_set.difference(
+        generated_coordinate_set)
+    coordinates_missing_in_license = generated_coordinate_set.difference(
+        license_binary_coordinate_set)
     if superfluous_coordinates_in_license:
-        print('Dependencies in LICENSE-binary that appear unused (may be shaded?): ')
-        # These don't fail the check, as storm-shaded-deps dependencies will appear here
+        print('Dependencies in LICENSE-binary that appear unused: ')
         for coord in superfluous_coordinates_in_license:
             print(coord)
     print('')
@@ -111,12 +144,15 @@ def check_license_binary():
         for coord in coordinates_missing_in_license:
             print(coord)
         return False
-    return True
-
+    return not coordinates_missing_in_license and not superfluous_coordinates_in_license
 
 
 with cd(project_root):
-    # check_dependency_licenses()
+    success = True
     # build_storm()
-    check_license_binary()
-    # build_storm_dist_binary()
+    generate_dependency_licenses()
+    success = success and check_dependency_licenses()
+    success = success and check_license_binary()
+    if not success:
+        exit(1)
+    exit(0)
